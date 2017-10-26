4c66ef4260e3777e5d5fbd928e76e170f2de2e0c
airt <airtial@gmail.com>
2017-10-26 17:51:43 +0800

    feat: renderer / pm service

--- src/view/app.component.ts
+++ src/view/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { PMService } from './pm.service'
 
 @Component({
   selector: 'app',
@@ -11,8 +12,41 @@ export class AppComponent implements OnInit {
 
   readonly versions = process.versions
 
+  constructor(private pmService: PMService) { }
+
   ngOnInit() {
     console.log('component initialized')
+
+    // -----
+
+    const handle = this.pmService.pm.createProcessHandle({
+      name: 'bash-t',
+      command: 'bash',
+      args: ['test/loop.sh'],
+    })
+
+    this.pmService.pm.startProcess(handle)
+
+    const process = this.pmService.pm.getProcessInstance(handle)
+
+    if (!process) throw new Error(`Process ${handle} start failed`)
+
+    console.log('process pid = ', process.pid)
+
+    process.stdout.on('data', (data) => {
+      console.log('[stdout]', data.toString())
+    })
+
+    process.stderr.on('data', (data) => {
+      console.log('[stderr]', data.toString())
+    })
+
+    process.on('close', (code, signal) => {
+      console.log('[process-close]', code, signal)
+    })
+
+    // -----
+
   }
 
 }
--- src/view/app.module.ts
+++ src/view/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
+import { PMService } from './pm.service'
 
 @NgModule({
   imports: [BrowserModule],
   declarations: [AppComponent],
+  providers: [PMService],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
